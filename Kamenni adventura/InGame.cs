b6c7d52a19f20674ed0f91f62e5a910805cb062d
KantaV <100676593+KantaV@users.noreply.github.com>
2023-01-21 17:43:07 +0100

    Otoceni postavy ve smeru pohybu

--- Kamenni adventura/InGame.cs
+++ Kamenni adventura/InGame.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Kamenni_adventura
 {
     public partial class InGame : UserControl
@@ -22,7 +23,7 @@ namespace Kamenni_adventura
 
         private int speed = 5,gravitace=4;
         private int predchozi=226;
-        private bool povolSkok = true;
+        private bool povolSkok = true,koukaVlevo=true;
 
         private void timer1_Tick(object sender, EventArgs e)
         {
@@ -33,22 +34,54 @@ namespace Kamenni_adventura
             }
             else povolSkok = true;
 
-            if(Form1.doleva&&pictureBoxPostava.Left>0) pictureBoxPostava.Left -= speed;
-            if(Form1.doprava && pictureBoxPostava.Left < 833) pictureBoxPostava.Left += speed;
-            if(Form1.nahoru && pictureBoxPostava.Top > 0&&povolSkok) pictureBoxPostava.Top -= speed * 15;
-            if(Form1.dolu&&pictureBoxPostava.Top<229)pictureBoxPostava.Top+= speed;
+
+            if (Form1.doleva && pictureBoxPostava.Left > 0)
+            {
+                koukaVlevo = true;
+                pictureBoxPostava.Left -= speed;
+            }
+            if (Form1.doprava && pictureBoxPostava.Left < 833)
+            {
+                koukaVlevo = false;
+                pictureBoxPostava.Left += speed;
+
+            }
+            if (Form1.nahoru && pictureBoxPostava.Top > 0 && povolSkok)
+            {
+                pictureBoxPostava.Top -= speed * 15;
+            }
+            if (Form1.dolu && pictureBoxPostava.Top < 229)
+            {
+                pictureBoxPostava.Top += speed;
+            }
             if (Form1.attack)
             {
                 delay = 100;
                 System.Media.SoundPlayer prehravac=new System.Media.SoundPlayer(Resources.Hank_rve);
                 prehravac.Play();
             }
-            if (delay > 0)
+
+            if(koukaVlevo)
             {
-                pictureBoxPostava.Image = Resources.hankAttack;
-                --delay;
+                pictureBoxPostava.Image = Resources.hank2;
+                if (delay > 0)
+                {
+                    pictureBoxPostava.Image = Resources.hankAttack;
+                    --delay;
+                }
             }
-            else pictureBoxPostava.Image = Resources.hank2;
+            else
+            {
+                pictureBoxPostava.Image = Resources.hank2;
+                pictureBoxPostava.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                if (delay > 0)
+                {
+                    pictureBoxPostava.Image = Resources.hankAttack;
+                    pictureBoxPostava.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    --delay;
+                }
+            }
+
 
             predchozi = pictureBoxPostava.Top;
             if (pictureBoxPostava.Top < 226)
